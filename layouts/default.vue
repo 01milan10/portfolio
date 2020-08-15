bcf6a0332ba11d94ec124469d4e5d408ac733d2f
01milan10 <kmilan604@gmail.com>
2020-08-15 14:36:15 +0545

    completed the mobile version of the design

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="antialiased">
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body,
+html {
+  @apply bg-gray-100;
+}
+</style>
